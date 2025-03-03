b55f1587956b4f0255834c73aee0bc6c95086793
7777Satish <onlinegamespartner2@gmail.com>
2025-03-04 01:14:47 +0530

    Connected to database

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,12 +1,22 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import styles from './Home.module.css';
 import Left from './Components/Left';
 import Movable from './Components/Movable';
 
 function Home(){
-
-    // const [darkmore, setDarkmode] = useState(1);
+    
+    const [data, setData] = useState(null);
     const [layout, setLayout] = useState(250);
+
+    useEffect( ()=>{
+        async function a(){
+            const res = await axios.get('http://localhost:4000/api');
+            setData(res.data.data);
+        }
+
+        a();
+    } )
 
     return <>
         <div className={styles.main} style={{gridTemplateColumns:`${layout}px 1fr`}}>
@@ -18,7 +28,9 @@ function Home(){
             <div className={styles.right}>
 
                 <div className={styles.top}></div>
-                <div className={styles.bottom}></div>
+                <div className={styles.bottom}>
+                    {data}
+                </div>
 
             </div>
         </div>
